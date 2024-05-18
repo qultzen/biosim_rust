a262b6c7cddc33084c4da86a07e976830637cb11
Kim Ly <kim.son.ly@nmbu.no>
2024-05-18 18:47:53 +0200

    Finnished restructuring

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use biosim_rust::Config;
+use std::process;
+
 fn main() {
-    println!("HelloWorld!")
+    let input_map = "
+        WWW
+        WHW
+        WWW"
+    .to_string();
+
+    let input_pop = vec![((1, 1), "Herbivore".to_string(), 200)];
+    let config = Config::build(input_map.clone(), input_pop.clone()).unwrap();
+
+    if let Err(e) = biosim_rust::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
